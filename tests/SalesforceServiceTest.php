e86895d7678515ff5c76dd7731dabdbeaaa15725
Morgan <morgan@synergitech.co.uk>
2023-03-10 13:17:11 +0000

    Start adding additional tests

---
+++ tests/SalesforceServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SynergiTech\Salesforce\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class SalesforceServiceTest extends TestCase
+{
+
+    public function testAuthenticate(): void
+    {
+
+    }
+
+}
---
+++ tests/TableServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SynergiTech\Salesforce\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class TableServiceTest extends TestCase
+{
+    public function testFind(): void
+    {
+        
+    }
+
+    public function testFindMany(): void
+    {
+
+    }
+}
